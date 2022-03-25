29ca26d45e2b14cc0b2d65b2b56642a24ba0302a
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2022-03-25 08:16:14 +0100

    First

--- src/main/java/com/example/tweeter/controllers/TweeterController.java
+++ src/main/java/com/example/tweeter/controllers/TweeterController.java
@@ -1,36 +1,23 @@
 package com.example.tweeter.controllers;
 
-import com.example.tweeter.models.Tweat;
 import com.example.tweeter.repositores.TestRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class TweeterController {
     private final TestRepository testRepo = new TestRepository();
 
     @GetMapping("/")
-    public String index(Model thingThatTransportDataToView){
-        List<Tweat> allTweats = testRepo.getAllTweats();
-        thingThatTransportDataToView.addAttribute("allMyTweatsVariable", allTweats);
+    public String index(){
         return "index";
     }
 
     @GetMapping("/test")
-    public String test(HttpSession session, Model model){
-        String helloWorld = (String) session.getAttribute("username");
-        model.addAttribute("message", helloWorld);
+    public String test(){
         return "test";
-    }
-
-    @PostMapping("/sessionTweat")
-    public String sessionExample(HttpSession session){
-        session.setAttribute("username","Hello World");
-        return "redirect:/test";
     }
 }
