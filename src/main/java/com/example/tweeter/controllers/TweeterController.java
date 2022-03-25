f3e08a1062a253d27c11211f384a5fd603384445
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2022-03-25 09:17:43 +0100

    First

--- src/main/java/com/example/tweeter/controllers/TweeterController.java
+++ src/main/java/com/example/tweeter/controllers/TweeterController.java
@@ -1,18 +1,21 @@
 package com.example.tweeter.controllers;
 
+import com.example.tweeter.models.Tweat;
 import com.example.tweeter.repositores.TestRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class TweeterController {
     private final TestRepository testRepo = new TestRepository();
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model m){
+        List<Tweat> listOfAllTweats = testRepo.getAllTweats();
+        m.addAttribute("allTweats",listOfAllTweats);
         return "index";
     }
 
